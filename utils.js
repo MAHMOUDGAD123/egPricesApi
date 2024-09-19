d607c35dc4ade4c3349ddbddf310a1ec4683577a
Mahmoud_Gad <hooda.gad333@gmail.com>
2024-09-19 19:14:02 +0300

    fixing a parser bug

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@
  * remove commas (,) from the text
  * @param {string} text
  */
-const parse_number_no_comma = (text) => {
+const parse_float_no_comma = (text) => {
   return Number.parseFloat(text.replace(",", ""));
 };
 
@@ -10,7 +10,7 @@ const parse_number_no_comma = (text) => {
  * text parser for all prices except live
  * @param {string} text
  */
-const prices_parser = (text) => parse_number_no_comma(text);
+const prices_parser = (text) => parse_float_no_comma(text);
 
 /**
  * text parser for live only
@@ -21,11 +21,13 @@ const live_parser = (text) => {
     ? text.slice(1, text.length - 1)
     : text.startsWith("+")
     ? text
-    : parse_number_no_comma(text);
+    : parse_float_no_comma(text);
 };
 
 const text_parser_map = new Map([
   ["prices", prices_parser],
+  ["gold", prices_parser],
+  ["silver", prices_parser],
   ["live", live_parser],
 ]);
 
